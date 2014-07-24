2d9bd801907d951083d0c0d44ed5a3b95c1cf2e7
Your Name <you@example.com>
2014-07-24 15:24:38 +0000

    made routes dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/img/1'=> 'application#first'
-  get '/img/2' => 'application#second'
-  get '/img/3' => 'application#third'
+  get '/img/:id' => 'application#show'
+
 end
